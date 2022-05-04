6e8b56c98c0d09825fe123ca763ecaa82949b3f9
wjallen <wallen@tacc.utexas.edu>
2022-05-04 11:45:33 -0500

    using argparse now

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,11 @@
 FROM ubuntu:18.04
 
-RUN apt-get update && \
-    apt-get -y upgrade && \
-    apt-get -y install python3
+RUN apt-get update && apt-get upgrade -y && apt-get install -y python3
 
-# if /code does not exist, it will make that folder
 COPY pi.py /code/pi.py
 
-RUN chmod +rx /code/pi.py
+RUN chmod +x /code/pi.py
 
 ENV PATH "/code:$PATH"
 
+CMD ["pi.py", "-h"]
--- pi.py
+++ pi.py
@@ -3,9 +3,14 @@ from random import random as r
 from math import pow as p
 from sys import argv
 
-# Make sure number of attempts is given on command line
-assert len(argv) == 2
-attempts = int(argv[1])
+# Use argparse to take command line options and generate help text
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("number", help="number of random points (int)", type=int)
+args = parser.parse_args()
+
+# Grab number of attempts from command line
+attempts = args.number
 inside = 0
 tries = 0
 
